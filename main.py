425eefc4c0df3e345e08305d97c68b326c8525d1
Jon Debonis <jon@trov.com>
2014-06-29 14:48:52 -0700

    Fixing imports.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
